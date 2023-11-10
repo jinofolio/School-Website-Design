9516def9111f9875565af42b325955b8d53812d4
jinofolio <148406534+jinofolio@users.noreply.github.com>
2023-11-10 09:06:29 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        popmer : ['Poppins', 'sans-serif'] ,
+        mont : ['Montserrat', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
